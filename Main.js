320e34119ecd2e4932a5d8fc2988a9ab944fde2d
VickriFahrurozi <VickriFahrurozi@gmail.com>
2022-09-12 16:49:18 +0700

    feat : fix error bismillah

--- Main.js
+++ Main.js
@@ -6,7 +6,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(cors());
-port = 3001;
+port = process.env.PORT || 3001;
 const mysql = require('mysql');
 
 const bodyParser = require('body-parser');
